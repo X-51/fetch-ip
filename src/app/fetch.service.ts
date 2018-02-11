89c0980601f83273a1e66c0b35d2a693a7a25c95
X-51 <daniel.rakoczy91@gmail.com>
2018-02-11 15:59:12 +0100

    indentation changes

--- src/app/fetch.service.ts
+++ src/app/fetch.service.ts
@@ -1,10 +1,12 @@
-import 'rxjs/add/operator/map'; // map method from rxjs library
-
 import {Injectable} from '@angular/core';
 import {Http, Response} from '@angular/http';
 import {Observable} from 'rxjs/Rx';
+
+import 'rxjs/add/operator/map'; // map method from rxjs library
+
 @Injectable()
 export class FetchService {
+  
 constructor(private http:Http) { }
   // Uses http.get() to load a single JSON file
   getData() {
